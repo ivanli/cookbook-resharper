ede786b1631ce30c1251a07404c0432ed1b8941c
Ivan Li <ivankyli@gmail.com>
2015-07-29 08:11:35 +1200

    Renamed windows package to its proper name

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,7 +9,8 @@
 
 include_recipe 'windows'
 
-windows_package 'Resharper 9' do
+windows_package 'JetBrains ReSharper Ultimate in Visual Studio 2013' do
+  package_name 'JetBrains ReSharper Ultimate in Visual Studio 2013'
   source "http://download.jetbrains.com/resharper/JetBrains.ReSharper#{node['resharper']['variant']}.#{node['resharper']['version']}.exe"
   installer_type :custom
   options "/SpecificProductNames=ReSharper /Silent=True /VsVersion=12"
